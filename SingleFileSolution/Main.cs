969ccce757780f0d9477c7c4b0f7805c5a940f03
Larry O'Brien <lobrien@knowing.net>
2012-11-06 12:14:43 -1000

    base

--- SingleFileSolution/Main.cs
+++ SingleFileSolution/Main.cs
@@ -7,31 +7,15 @@ using MonoTouch.UIKit;
 
 namespace SingleFileSolution
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the 
-	// User Interface of the application, as well as listening (and optionally responding) to 
-	// application events from iOS.
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		// class-level declarations
 		UIWindow window;
 		
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this 
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
-			// create a new window instance based on the screen size
 			window = new UIWindow (UIScreen.MainScreen.Bounds);
 			
-			// If you have defined a view, add it here:
-			// window.AddSubview (navigationController.View);
-			
-			// make the window visible
 			window.MakeKeyAndVisible ();
 			
 			return true;
@@ -40,11 +24,8 @@ namespace SingleFileSolution
 
 	public class Application
 	{
-		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
 		}
 	}
