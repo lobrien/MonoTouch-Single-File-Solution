0f2d74d528dea9d5c7b5d712a1c893c960b0cc7c
Larry O'Brien <lobrien@knowing.net>
2012-11-16 13:20:43 -1000

    base

--- SingleFileSolution/Main.cs
+++ SingleFileSolution/Main.cs
@@ -25,35 +25,34 @@ namespace SingleFileSolution
 			var frameDescription = String.Format ("ContentView frame is [{0},{1}],[{2},{3}]", Frame.Left, Frame.Top, Frame.Right, Frame.Bottom);
 			
 			var ulTolr = new PointF[] { rect.Location, rect.Location + rect.Size };
-			var urToll = new PointF[] { new PointF(rect.Right, rect.Top), new PointF(rect.Left, rect.Bottom) };
-			using(var g = UIGraphics.GetCurrentContext ())
-			{
+			var urToll = new PointF[] { new PointF (rect.Right, rect.Top), new PointF (rect.Left, rect.Bottom) };
+			using (var g = UIGraphics.GetCurrentContext ()) {
 				//Fill the view with color
-				fillColor.SetFill();
-				g.FillRect(rect);
+				fillColor.SetFill ();
+				g.FillRect (rect);
 				
 				//Draw diagonals to indicate off-screen geometry
 				var red = UIColor.Red;
-				red.SetFill();
-				red.SetStroke();
-				g.AddLines(ulTolr);
-				g.AddLines(urToll);
-				g.DrawPath(CGPathDrawingMode.Stroke);
+				red.SetFill ();
+				red.SetStroke ();
+				g.AddLines (ulTolr);
+				g.AddLines (urToll);
+				g.DrawPath (CGPathDrawingMode.Stroke);
 				
 				//Show some explanatory text
-				g.ScaleCTM(1f,-1f);
-				g.SelectFont("Arial",16f,CGTextEncoding.MacRoman);
-				UIColor.Black.SetFill();
-				g.SetTextDrawingMode(CGTextDrawingMode.Fill);
-				g.ShowTextAtPoint(10,-50,frameDescription);
-				g.ShowTextAtPoint(10,-75,extraText);
+				g.ScaleCTM (1f, -1f);
+				g.SelectFont ("Arial", 16f, CGTextEncoding.MacRoman);
+				UIColor.Black.SetFill ();
+				g.SetTextDrawingMode (CGTextDrawingMode.Fill);
+				g.ShowTextAtPoint (10, -50, frameDescription);
+				g.ShowTextAtPoint (10, -75, extraText);
 			}
 		}
 	}
 
-	public class UIScrollViewRemarksViewController : UIViewController
+	public class SimpleViewController : UIViewController
 	{
-		public UIScrollViewRemarksViewController () : base ()
+		public SimpleViewController () : base ()
 		{
 		}
 		
@@ -67,36 +66,9 @@ namespace SingleFileSolution
 		{
 			base.ViewDidLoad ();
 			
-			//Create a ScrollView with a ContentSize sized to desired scrolling extents
-			var scrollView = new UIScrollView();
-			scrollView.ContentSize = new SizeF(500,500);
-			scrollView.ZoomScale = 1.0f;
-			
-			//Create a series of content views that will show various aspects of scrolling
-			//Basic view; neatly aligned
-			var contentView0 = new ContentView(UIColor.Green, "Normal case: neatly aligned");
-			contentView0.Frame = new RectangleF(0, 0, 300, 300);
-			
-			//View with area not reachable by scrolling. Also creates "gap" between contentView1 and contentView2
-			var contentView1 = new ContentView(UIColor.Purple, "Frame extends outside of ContentSize");
-			contentView1.Frame = new Rectangle(300, -20, 300, 300);
-			
-			//Basic view; neatly aligned. Sized to require scrolling but aligned with scrollView.ContentSize
-			var contentView2 = new ContentView(UIColor.Yellow, "Note how you can't scroll from gap (black) between views");
-			contentView2.Frame = new Rectangle(0, 300, 500, 200);
-			
-			scrollView.AddSubview(contentView0);
-			scrollView.AddSubview(contentView1);
-			scrollView.AddSubview(contentView2);
-			this.View = scrollView;
-			
-			scrollView.Scrolled += (s,e) => {
-				Console.WriteLine (String.Format ("ContentOffset = {0}", scrollView.ContentOffset));
-			};
-			
-			scrollView.DraggingStarted += (object sender, EventArgs e) => {
-				Console.WriteLine ("Scrolling started");
-			};
+			var view = new ContentView (UIColor.Blue, "A View");
+		
+			this.View = view;
 		}
 	}	
 
@@ -104,13 +76,13 @@ namespace SingleFileSolution
 	public  class AppDelegate : UIApplicationDelegate
 	{
 		UIWindow window;
-		UIScrollViewRemarksViewController viewController;
+		SimpleViewController viewController;
 
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			window = new UIWindow (UIScreen.MainScreen.Bounds);
 
-			viewController = new UIScrollViewRemarksViewController ();
+			viewController = new SimpleViewController ();
 			window.RootViewController = viewController;
 
 			window.MakeKeyAndVisible ();
